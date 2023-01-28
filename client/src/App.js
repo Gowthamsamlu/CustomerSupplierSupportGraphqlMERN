b71b871e9c6f35b105853427a589611f07b0c5d9
Gowtham Balaji <gowthamsamlu@gmail.com>
2023-01-28 07:47:34 +0530

    Backend milestone

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className='App w-full border-2 border-r-black'>
+      <div className='container bg-orange-200 w-full'>
+        <h1 class='text-3xl font-bold underline'>Hello world!</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
