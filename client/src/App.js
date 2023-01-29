9dffac40c94aac69961de2434fb71a1ec38c8328
Gowtham Balaji <gowthamsamlu@gmail.com>
2023-01-29 14:52:36 +0530

    Tailwindcss Config {content}

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,18 @@
+import { Login } from "./components";
+
 function App() {
-  return (
-    <div className='App w-full border-2 border-r-black'>
-      <div className='container bg-orange-200 w-full'>
-        <h1 class='text-3xl font-bold underline'>Hello world!</h1>
+  const demoPage = false;
+
+  return demoPage ? (
+    <div className='App h-screen max-h-screen max-w-screen-3xl border-4 2xl:border-blue-900 border-red-900 bg-red-300 lg:bg-yellow-300 2xl:bg-green-300 3xl:bg-blue-300 xl:bg-orange-200'>
+      <div className='container'>
+        <h1 className='text-3xl font-bold underline'>Hello worlds!</h1>
+      </div>
+    </div>
+  ) : (
+    <div className='App h-screen max-h-screen w-screen max-w-screen bg-gray-300'>
+      <div className='container'>
+        <Login />
       </div>
     </div>
   );
---
+++ client/src/components/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className='flex pt-8 w-full border-4 border-black'>
+      <div className=''>dadasda</div>
+      <div className=''>dadasda</div>
+    </div>
+  );
+};
+
+export default Login;
